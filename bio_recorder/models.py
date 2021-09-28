0e98d5e57c3bbe0af5002eff1cac1d69ce1408dd
joekbullard <joekbullard@gmail.com>
2021-09-28 19:50:13 +0000

    initial model

--- bio_recorder/models.py
+++ bio_recorder/models.py
@@ -1,3 +1,37 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+
+        
+
+class Recorder(models.Model):
+    recorder_id = models.AutoField(primary_key=True)
+    recorder_user = models.CharField(max_length=20, blank=True, null=True)
+    recorder_first_name = models.CharField(max_length=20, blank=True, null=True)
+    recorder_last_name = models.CharField(max_length=30, blank=True, null=True)
+    recorder_email = models.CharField(max_length=100, blank=True, null=True)
+    recorder_join_date = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return self.recorder_user
+    
+    class Meta:
+        verbose_name = 'recorder'
+
+
+class Records(models.Model):
+    record_id = models.AutoField(primary_key=True)
+    recorder = models.ForeignKey(Recorder, models.DO_NOTHING, db_column='recorder', blank=True, null=True)
+    species = models.TextField(max_length=100, blank=False, null=False)
+    record_date = models.DateField()
+    notes = models.CharField(max_length=255, blank=True, null=True)
+    geom = models.PointField(srid=27700)
+
+    def __int__(self):
+        return self.record_id
+
+    class Meta:
+        verbose_name = 'record'
+
+
+
+
